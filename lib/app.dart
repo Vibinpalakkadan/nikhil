5cfbeea2a4be0ee4089b906768fa13f9ede9d5d5
Nikhz <nikhil141p@gmail.com>
2020-11-09 14:17:36 +0530

    Car dealer project_flutter

--- lib/app.dart
+++ lib/app.dart
@@ -1,82 +1,152 @@
+//app.dart
+//my "CAR"t  project
 
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
-//import './widgets/buttonSamples.dart';
+import './widgets/buttonSamples.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Mallu Developer",
-        home: Scaffold(
-          body: Container(
-            child: Center(
-              child: TextField(
-                keyboardType: TextInputType.text,
-                onChanged: (String textValue) {
-                   print("Output: " + textValue);
-                },
-                enabled: true, //we can disable Keyboard by giving false instead of true boolean here
-                controller: TextEditingController(text:"Default Values"),
-                decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.lightBlueAccent,
-                          width: 2,
-                      ),
-                      borderRadius: BorderRadius.circular(20),
-                      ),
+      title: "Mallu Developer",
+      home: Scaffold(
+        body: ListView(
+          children: [
+            ButtonSamples(),
+            ButtonSamples(),
+          ], //children
+        ),
 
-                      disabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.red,
-                          width: 4,
-                        ),
-                        borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(30),
-                        topRight: Radius.circular(28),
-                        bottomLeft: Radius.circular(23),
-                        bottomRight: Radius.circular(19),
-                        ),
-                      ),
-
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.orange,
-                      width: 3,
-                    ),
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(23),
-                        topRight: Radius.circular(23),
-                        bottomRight: Radius.circular(23),
-                        bottomLeft: Radius.circular(23),
-                    ),
-                  ),
-                  icon: Icon(Icons.account_box,color: Colors.green),
-                  suffixIcon: Icon(Icons.more),
-                  contentPadding: EdgeInsets.all(20),
-                  labelText: "User name",
-                  labelStyle: TextStyle(
-                    color: Colors.yellow,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  hintText: "eg: Michael",
-                  hintStyle: TextStyle(
-                    color: Colors.pink,
-                    fontSize: 20,
-                    fontWeight: FontWeight.normal,
-                  ),
-                  helperText: "Enter your User Name",
-                  helperStyle: TextStyle(
-                    color: Colors.red,fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
+        backgroundColor: Colors.white70,
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          elevation: 10, //3D effect
+          title: Text("Mallu Developer App"),
+          //leading: IconButton(
+          //  onPressed: () {
+          //  print("Leading button clicked");
+          //},
+          //icon: Icon(Icons.menu),
+          //  ),
+        ),
+        floatingActionButton: FlatButton(
+          onPressed: (){
+            print("Floating Action Button clicked");
+          },
+          child: Icon(Icons.add_a_photo),
+          color: Colors.red,
+          shape: CircleBorder(),
+          padding: EdgeInsets.all(10),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              DrawerHeader(
+                child: Text("Car Circle"),
+                decoration: BoxDecoration(
+                  color: Colors.lightBlueAccent,
                 ),
               ),
-            ),
-
-            ),
+              ListTile(
+                title:  Text("Favourites"),
+                subtitle: Text("Click to Go to favourite page"),
+                onTap: () {
+                  print("Go to favourite page");
+                },
+                onLongPress: () {
+                  print("Long Press Happened");
+                },
+                leading: Icon(Icons.favorite),
+                trailing: Icon(Icons.arrow_forward),
+              ),
+              ListTile(
+                title:  Text("Sedan Models"),
+                subtitle: Text("Click to Go to Sedan Model Cars"),
+                onTap: () {
+                  print("Go to Sedan Model Cars");
+                },
+                onLongPress: () {
+                  print("Long Press Happened");
+                },
+                leading: Icon(Icons.directions_car),
+                trailing: Icon(Icons.arrow_forward),
+              ),
+              ListTile(
+                title:  Text("Hatch back model"),
+                subtitle: Text("Click to Go to Hatch back Model Cars"),
+                onTap: () {
+                  print("Go to Hatch Model Cars");
+                },
+                onLongPress: () {
+                  print("Long Press Happened");
+                },
+                leading: Icon(Icons.directions_car),
+                trailing: Icon(Icons.arrow_forward),
+              ),
+              ListTile(
+                title:  Text("We can Wash"),
+                subtitle: Text("Book to Wash your Car"),
+                onTap: () {
+                  print("Car Wash Booking");
+                },
+                onLongPress: () {
+                  print("Long Press Happened");
+                },
+                leading: Icon(Icons.local_car_wash),
+                trailing: Icon(Icons.arrow_forward),
+              ),
+            ],
           ),
+        ),
+        endDrawer: Drawer(
+          child: ListView(
+            children: [
+              DrawerHeader(
+                child: Text("My App"),
+                decoration: BoxDecoration(
+                  color: Colors.lightBlueAccent,
+                ),
+              ),
+              ListTile(
+                title:  Text("Profile"),
+                subtitle: Text("Click to Go to Profile"),
+                onTap: () {
+                  print("Go to Profile page");
+                },
+                onLongPress: () {
+                  print("Long Press Happened");
+                },
+                leading: Icon(Icons.perm_identity),
+                trailing: Icon(Icons.arrow_forward),
+              ),
+              ListTile(
+                title:  Text("Accounts"),
+                subtitle: Text("Click to Go to Accounts"),
+                onTap: () {
+                  print("Go to Accounts");
+                },
+                onLongPress: () {
+                  print("Long Press Happened");
+                },
+                leading: Icon(Icons.account_box),
+                trailing: Icon(Icons.arrow_forward),
+              ),
+              ListTile(
+                title:  Text("Settings"),
+                subtitle: Text("Click to Go to Settings"),
+                onTap: () {
+                  print("Go to Settings");
+                },
+                onLongPress: () {
+                  print("Long Press Happened");
+                },
+                leading: Icon(Icons.settings),
+                trailing: Icon(Icons.arrow_forward),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
-   }
+  }
 }
